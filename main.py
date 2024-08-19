12c55ad6d5aeafd50751ac2a763594ba897a202e
UzCoder123 <salohiddinsalohiddin345@gmail.com>
2024-08-19 14:53:55 +0500

    main.py

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
-from ursina import *
+from ursina import*
 
 app=Ursina()
-
-player=Entity(model='cube', scale_x=1, color=color.red, texture='white_cube.png', collider='box')
-ground=Entity(model='cube', scale_x=10, scale_z=10, texture='grass.png', y=-1, collider='box')
-
-def update():
-    if held_keys['a']:
-        player.x-=0.08
-    if held_keys['d']:
-        player.x+=0.08
-    if held_keys['s']:
-        player.z-=0.08
-    if held_keys['w']:
-        player.z+=0.08
-    camera.position=player.position
-    camera.y=3
-    camera.z=player.z-7
-    camera.rotation_x=20
 
 app.run()
